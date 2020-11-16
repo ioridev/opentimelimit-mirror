30af12ff079585b8f691d13cc87f00ecb2906506
Jonas Lochmann <git@inkompetenz.org>
2020-11-16 01:00:00 +0100

    Disable system lockdown if not configured after a reboot

--- app/src/main/java/io/timelimit/android/logic/BackgroundTaskLogic.kt
+++ app/src/main/java/io/timelimit/android/logic/BackgroundTaskLogic.kt
@@ -75,6 +75,8 @@ class BackgroundTaskLogic(val appLogic: AppLogic) {
 
             if (appLogic.deviceEntryIfEnabled.waitForNullableValue() != null) {
                 appLogic.platformIntegration.setEnableSystemLockdown(true)
+            } else {
+                appLogic.platformIntegration.setEnableSystemLockdown(false)
             }
         }
 
